26e6db9f272060b4874e19b180d739336cd71d0c
Hidenori Tanaka <tanaka8@stanford.edu>
2021-05-12 09:21:53 +0900

    don't save pt files

--- Experiments/singleshot.py
+++ Experiments/singleshot.py
@@ -15,16 +15,16 @@ def run(args):
 
     ## Data ##
     print('Loading {} dataset.'.format(args.dataset))
-    input_shape, num_classes = load.dimension(args.dataset) 
+    input_shape, num_classes = load.dimension(args.dataset)
     prune_loader = load.dataloader(args.dataset, args.prune_batch_size, True, args.workers, args.prune_dataset_ratio * num_classes)
     train_loader = load.dataloader(args.dataset, args.train_batch_size, True, args.workers)
     test_loader = load.dataloader(args.dataset, args.test_batch_size, False, args.workers)
 
     ## Model, Loss, Optimizer ##
     print('Creating {}-{} model.'.format(args.model_class, args.model))
-    model = load.model(args.model, args.model_class)(input_shape, 
-                                                     num_classes, 
-                                                     args.dense_classifier, 
+    model = load.model(args.model, args.model_class)(input_shape,
+                                                     num_classes,
+                                                     args.dense_classifier,
                                                      args.pretrained).to(device)
     loss = nn.CrossEntropyLoss()
     opt_class, opt_kwargs = load.optimizer(args.optimizer)
@@ -34,26 +34,26 @@ def run(args):
 
     ## Pre-Train ##
     print('Pre-Train for {} epochs.'.format(args.pre_epochs))
-    pre_result = train_eval_loop(model, loss, optimizer, scheduler, train_loader, 
+    pre_result = train_eval_loop(model, loss, optimizer, scheduler, train_loader,
                                  test_loader, device, args.pre_epochs, args.verbose)
 
     ## Prune ##
     print('Pruning with {} for {} epochs.'.format(args.pruner, args.prune_epochs))
     pruner = load.pruner(args.pruner)(generator.masked_parameters(model, args.prune_bias, args.prune_batchnorm, args.prune_residual))
     sparsity = 10**(-float(args.compression))
-    prune_loop(model, loss, pruner, prune_loader, device, sparsity, 
+    prune_loop(model, loss, pruner, prune_loader, device, sparsity,
                args.compression_schedule, args.mask_scope, args.prune_epochs, args.reinitialize, args.prune_train_mode, args.shuffle, args.invert)
 
-    
+
     ## Post-Train ##
     print('Post-Training for {} epochs.'.format(args.post_epochs))
-    post_result = train_eval_loop(model, loss, optimizer, scheduler, train_loader, 
-                                  test_loader, device, args.post_epochs, args.verbose) 
+    post_result = train_eval_loop(model, loss, optimizer, scheduler, train_loader,
+                                  test_loader, device, args.post_epochs, args.verbose)
 
     ## Display Results ##
     frames = [pre_result.head(1), pre_result.tail(1), post_result.head(1), post_result.tail(1)]
     train_result = pd.concat(frames, keys=['Init.', 'Pre-Prune', 'Post-Prune', 'Final'])
-    prune_result = metrics.summary(model, 
+    prune_result = metrics.summary(model,
                                    pruner.scores,
                                    metrics.flop(model, input_shape, device),
                                    lambda p: generator.prunable(p, args.prune_batchnorm, args.prune_residual))
@@ -72,8 +72,6 @@ def run(args):
         pre_result.to_pickle("{}/pre-train.pkl".format(args.result_dir))
         post_result.to_pickle("{}/post-train.pkl".format(args.result_dir))
         prune_result.to_pickle("{}/compression.pkl".format(args.result_dir))
-        torch.save(model.state_dict(),"{}/model.pt".format(args.result_dir))
-        torch.save(optimizer.state_dict(),"{}/optimizer.pt".format(args.result_dir))
-        torch.save(scheduler.state_dict(),"{}/scheduler.pt".format(args.result_dir))
-
-
+        #torch.save(model.state_dict(),"{}/model.pt".format(args.result_dir))
+        #torch.save(optimizer.state_dict(),"{}/optimizer.pt".format(args.result_dir))
+        #torch.save(scheduler.state_dict(),"{}/scheduler.pt".format(args.result_dir))
